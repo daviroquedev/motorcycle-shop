5082e2fbd136267f6bd9c14e338477e462f4b660
Davi Roque <sowdavi@gmail.com>
2022-07-03 03:17:49 -0300

    AddMotosDataBase

---
+++ dados/dados.js
@@ -0,0 +1,38 @@
+import Honda from '../assets/motos/Honda.png';
+import Bmw from '../assets/motos/bmw.png';
+import Hornet from '../assets/motos/hornet.png';
+ 
+ export const motos = {
+    CB500: {
+      moto: 'CB500',
+      modelo: '500F',
+      price: 'R$ 39.100',
+      state: '1.200 KM',
+      img: Honda,
+    },
+    BMW: {
+      moto: 'BMW',
+      price: 'R$ 158.500',
+      modelo: 'S1000RR',
+      state: '0 KM',
+      img: Bmw,
+    },
+
+    HORNET: {
+      moto: 'Hornet',
+      modelo: '2007',
+      price: 'R$ 25.600',
+      state: '1.200 KM',
+      img: Hornet,
+    },
+    KAWASAQUI: {
+      moto: 'kawasaqui',
+      modelo: '2020',
+      price: 'R$ 22.900',
+      state: '0 KM',
+      img: {
+        uri: 'https://s2.glbimg.com/QT6ukXRNkwt7u_j0J6Zs2prRMiQ=/0x0:921x691/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2022/y/4/lDfqbPTBW4RuE8iJbDAw/3ebfveyae5bldcygx5jyo2epzu.jpg',
+      },
+    },
+  };
+
